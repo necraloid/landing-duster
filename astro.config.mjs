6ddc81d5e703a18d2bcc09988d21fb8be0ade4bc
Erick Sebastián <esprado@sudamericano.edu.ec>
2026-08-25 22:09:31 -0500

    Add /landing as root for landing page in build params

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 import remarkReadingTime from "remark-reading-time";
 
 export default defineConfig({
-  site: "https://ericsebb.qzz.io/",
+  site: "https://ericsebb.qzz.io/landing",
   integrations: [sitemap(), icon()],
   markdown: {
     remarkPlugins: [
